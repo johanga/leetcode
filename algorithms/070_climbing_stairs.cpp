bcca90bb03d878d3ba1c09fe10e87da20f0e236d
Alex Kopytov <alex.kopytov@gmail.com>
2016-06-14 22:54:30 +0300

    Added Binet's formula solution to 070.

--- algorithms/070_climbing_stairs.cpp
+++ algorithms/070_climbing_stairs.cpp
@@ -1,3 +1,6 @@
+// You are climbing a stair case. It takes n steps to reach to the top.
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
 class Solution 
 {
 public:
@@ -24,5 +27,8 @@ public:
         
         // Recursive solution that is not accepted because of "Time Limit Exceed"
         // return climbStairs( n-1 ) + climbStairs( n-2 );
+        
+        // Binet's formula
+        // return static_cast< int >( ( pow( ( 1.0 + sqrt(5.0) ) / 2.0, n + 1 ) - pow( ( 1.0 - sqrt(5.0) ) / 2.0, n + 1 ) ) / sqrt(5.0) );
     }
 };
